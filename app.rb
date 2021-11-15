cada6e6d001aa33f3376dc4355cda71be46727fd
Samantha Diaz <samidiaz3@gmail.com>
2021-11-15 16:05:54 -0500

    commit tree af3aac0727956b271264e9ed20fd3d9aded9553b

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+     erb :user_input
+    end
+
+    post '/results' do 
+        word = PigLatinizer.new
+        @piglatin = word.piglatinize(params[:user_input])
+        erb :results
+    end
 end
